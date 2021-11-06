493862581529c5a5d7e91c2729b1673894840946
Carlos Rangel <charlesleimaginant@gmail.com>
2021-11-06 14:59:45 -0600

    Add functionalities to choose number of squares per side by an popup and random color for the divs.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,41 +3,65 @@ console.log('It works!');
 /* Todo */
 /* Selecciono el container */
 const container = document.querySelector('#mainContainer');
+let hue = 0;
 
 
 // Hacer la cuadrícula
-for (let step = 0; step < 256; step++) {
-    // Runs 15 times, with values of step 0 through 16
-    const div = document.createElement('div');
-    // Darle estílos  la cuadrícula acomodar la cuadrícula en el rectángulo
-    div.style.border = '1px solid grey';
-    div.style.width = '38px';
-    div.style.height = '38px';
-    div.classList.add('etch');
-    // metemos el div en el container
-    container.appendChild(div);
-    console.log(`Hecho el div número ${step}`);
+function makeGrid(squares = 16) {
+    const ladoDiv = 640/squares - 2;
+    const stepGrid = squares ** 2;
+    
+    for (let step = 0; step < stepGrid; step++) {
+        // Runs 15 times, with values of step 0 through 16
+        const div = document.createElement('div');
+        // Darle estílos  la cuadrícula acomodar la cuadrícula en el rectángulo
+        div.style.border = '1px solid grey';
+        div.style.width = `${ladoDiv}px`;
+        div.style.height = `${ladoDiv}px`;
+        div.classList.add('etch');
+        // metemos el div en el container
+        container.appendChild(div);
+        /* console.log(`Hecho el div número ${step}`); */
+        // llamar función para pintar divs
+        changeDivs();
+    }
 }
 
-// Hacer que el mouse cambie el estilo de un cuadro al pasar por encima - ¿cuándo haga clic o solo pasando el cursor?
-const divs = document.querySelectorAll('.etch');
-
-divs.forEach((div) => {
-    div.addEventListener('mouseenter', function(e) {
-        console.count(e.currentTarget);
-        e.currentTarget.style.backgroundColor = 'white';
-        /* console.log(this);
-        this.style.backgroundColor = 'green'; */
-    });
-});
-
-
 // botón que limpie el grid
-const clearButton = document.querySelector('#clear');
+/* const clearButton = document.querySelector('#clear');
 
 clearButton.addEventListener('click', () => {
     // Cuando damos click en el botón limpia los divs
     divs.forEach((div) => {
         div.style.backgroundColor = 'transparent';
     });
-});
+}); */
+
+// send a pop up asking for the number of squares per side
+function numberOfSquares() {
+    const squares = prompt("Enter the number of squares per side for the new grid: ", "10");
+    // Antes de hacer más divs hay que eliminar los que ya están
+    const divs = document.querySelectorAll('.etch');
+    divs.forEach((div) => {
+        div.remove();
+    });
+    makeGrid(Number(squares))
+}
+
+
+function changeDivs() {
+    // Hacer que el mouse cambie el estilo de un cuadro al pasar por encima - ¿cuándo haga clic o solo pasando el cursor?
+    const divs = document.querySelectorAll('.etch');
+    
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', function(e) {
+            /* console.count(e.currentTarget); */
+            hue += 1;
+            e.currentTarget.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+            /* console.log(this);
+            this.style.backgroundColor = 'green'; */
+        });
+    });
+}
+
+makeGrid();
